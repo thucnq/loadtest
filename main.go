c21113b4c624b0c820c848fbec9ff90a59870ced
Thuc Nguyen <thucnguyenquy@gmail.com>
2020-02-06 16:34:36 +0700

    third commit

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"mime/multipart"
 	"os"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
 
@@ -20,16 +22,43 @@ import (
 var counter int64
 
 var url string = "http://172.16.0.30:8080/v3.0/media"
+var contentType string = "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
 
 // var url string = "http://127.0.0.1:8080/v3.0/media"
-var AntsSize int = 500
-var n int = 100000
+var AntsSize int = 2000
+var n int = 1
 
 // var ch = make(chan byte)
+func postman() {
+	url := "http://172.16.0.30:8080/v3.0/media"
+
+	payload := strings.NewReader("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"source\"\r\n\r\naws\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"category\"\r\n\r\ndefault\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"format\"\r\n\r\njpg\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
+
+	req, _ := http.NewRequest("POST", url, payload)
+
+	req.Header.Add("content-type", "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW")
+	// req.Header.Add("cache-control", "no-cache")
+	// req.Header.Add("postman-token", "b6f97535-855f-2966-00ed-60b2f67b6e5f")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
+}
 
 func f(file *os.File) error {
 
 	http.DefaultTransport.(*http.Transport).DisableKeepAlives = true
+	// http.DefaultTransport.(*http.Transport).ResponseHeaderTimeout = time.Millisecond
+	// http.DefaultTransport.(*http.Transport).DialContext = (&net.Dialer{Timeout: time.Nanosecond}).DialContext
+	// http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{
+	// 	MaxVersion:         tls.VersionTLS11,
+	// 	InsecureSkipVerify: true,
+	// }
+	defer http.DefaultTransport.(*http.Transport).CloseIdleConnections()
 
 	// Buffer to store our request body as bytes
 	var requestBody bytes.Buffer
@@ -101,12 +130,15 @@ func f(file *os.File) error {
 		return err
 	}
 	// We need to set the content type from the writer, it includes necessary boundary as well
+	// fmt.Println(multiPartWriter.FormDataContentType())
 	req.Header.Set("Content-Type", multiPartWriter.FormDataContentType())
 
 	// Do the request
 	client := &http.Client{}
 	resp, err := client.Do(req)
+	fmt.Println(resp)
 	if err != nil {
+		fmt.Println(err)
 		return err
 	}
 	defer resp.Body.Close()
@@ -114,7 +146,8 @@ func f(file *os.File) error {
 }
 
 func main() {
-
+	// postman()
+	// return
 	// Limit the number of spare OS threads to just 1
 	runtime.GOMAXPROCS(6)
 
